3232cef80688e73162c8caeab5f609a4307b24c0
Henrik Olsson <henrik@fixme.se>
2013-03-11 20:56:49 +0100

    Move Unbuffured to utils

--- bliss/utils.py
+++ bliss/utils.py
@@ -1,4 +1,16 @@
 import re
 
 def escape_generic(s):
-    return re.sub(r"[^\w]", "_", s, flags=re.IGNORECASE+re.UNICODE)
+    return re.sub(r"[^\w-]", "_", s, flags=re.IGNORECASE+re.UNICODE)
+
+class Unbuffered:
+    def __init__(self, stream):
+        self.stream = stream
+        
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+        
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+       
