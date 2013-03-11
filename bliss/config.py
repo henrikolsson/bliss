bde22afb91666aec902089b80bf9a936f0be26f0
Henrik Olsson <henrik@fixme.se>
2013-03-11 20:54:36 +0100

    Moved more config to seperate module

--- bliss/config.py
+++ bliss/config.py
@@ -1,4 +1,5 @@
 import logging
+import couchdb
 
 fh = logging.FileHandler('bliss.log')
 fh.setLevel(logging.DEBUG)
@@ -10,6 +11,29 @@ ch.setFormatter(formatter)
 logging.getLogger('').addHandler(fh)
 logging.getLogger('').addHandler(ch)
 logging.getLogger('').setLevel(logging.DEBUG)
+logging.getLogger('apiclient').setLevel(logging.WARN)
 
 DEBUG = True
 DB = "bliss"
+BASEDIR = '/mnt/pub/movies'
+RT_KEY = 'X'
+FB_KEY = 'Y'
+
+couch = couchdb.Server()
+if not DB in couch:
+    db = couch.create(DB)
+    movie_views = {
+        "_id":"_design/movie",
+        "language": "python",
+        "views":
+            {
+            "all": {
+                "map": """def fun(doc):
+    if doc['type'] == 'movie':
+        yield doc['_id'], doc"""
+                }
+            }
+        }
+    db.save(movie_views)
+else:
+    db = couch[DB]
